fc7f578d9eca525572a45f6f6789bd6b59bcb744
Тагир Сахабиев <killerbeesexy@gmail.com>
2025-03-21 12:27:35 +0300

    balbal

--- app/app/settings/default.py
+++ app/app/settings/default.py
@@ -13,7 +13,7 @@ SECRET_KEY = "hehe"
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["localhost", '0.0.0.0']
+ALLOWED_HOSTS = ["localhost", '0.0.0.0', 'http://0.0.0.0:8000']
 CSRF_TRUSTED_ORIGINS = ["http://localhost:8000"]
 
 # Application definition
